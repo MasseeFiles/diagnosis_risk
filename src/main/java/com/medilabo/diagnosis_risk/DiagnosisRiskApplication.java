8724820919dc2dbf6daca08d9550e97248b5eed7
TotoLanson <jbmassee@gmail.com>
2024-07-24 10:13:24 +0200

    Commit initial

---
+++ src/main/java/com/medilabo/diagnosis_risk/DiagnosisRiskApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.diagnosis_risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisRiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/diagnosis_risk/DiagnosisRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.diagnosis_risk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
